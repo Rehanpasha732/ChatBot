eee16290f1efa2dfad5e920c97e0d5efbd434864
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2023-07-07 03:32:17 -0700

    chatBot

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatComponent from './ChatComponent';
+function App() {
+  return (
+    <div className="App">
+      <ChatComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ChatComponent.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import { AiOutlineSend, AiFillRobot } from 'react-icons/ai';
+import { FaRobot } from 'react-icons/fa';
+import { GiRobotGolem } from 'react-icons/gi';
+
+const ChatComponent = () => {
+    const [userQuestions, setUserQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState("");
+    const [responses, setResponses] = useState([]);
+    const [loading, setLoading] = useState(false); // Added loading state
+
+    const Chat = async () => {
+        setLoading(true); // Show loader
+
+        const configuration = new Configuration({
+            apiKey: "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",
+        });
+
+        const openai = new OpenAIApi(configuration);
+
+        async function chat(input) {
+            const messages = [{ role: "user", content: input }];
+            const response = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: messages,
+                temperature: 0,
+            });
+            return response.data.choices[0].message.content;
+        }
+
+        const promptTemplate = `
+      Be answer in roman urdu
+      Question: {question}
+    `;
+        const prompt = promptTemplate.replace("{question}", currentQuestion);
+
+        try {
+            const response = await chat(prompt);
+            setUserQuestions((prevQuestions) => [
+                ...prevQuestions,
+                currentQuestion,
+            ]);
+            setResponses((prevResponses) => [...prevResponses, response]);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false); // Hide loader
+        }
+    };
+
+    const handleQuestionChange = (e) => {
+        setCurrentQuestion(e.target.value);
+    };
+
+    const handleButtonClick = () => {
+        if (currentQuestion) {
+            Chat();
+            setCurrentQuestion("");
+        }
+    };
+
+    return (
+        <>
+            <div className="chatbot_div">
+                <div className="header">
+                    <h1> <GiRobotGolem /> ChatBot</h1>
+                </div>
+                <div className="chatting_div">
+                    <div className="chat_box">
+                        {userQuestions.map((question, index) => (
+                            <>
+                                <div className="chat_box_question">{question}</div>
+                                <div className="chat_box_answer">
+                                    <FaRobot color="white" /> {responses[index]}
+                                </div>
+                            </>
+                        ))}
+                        {loading && ( // Render loader if loading is true
+                            <div class="loader"></div>
+                        )}
+                    </div>
+                    <div className="inputDiv">
+                        <input
+                            className="Userinput"
+                            id="questionInput"
+                            type="text"
+                            value={currentQuestion}
+                            onChange={handleQuestionChange}
+                            placeholder="Send a message"
+                        />
+                        <AiOutlineSend
+                            onClick={handleButtonClick}
+                            color="white"
+                            size={20}
+                            className="send_btn"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ChatComponent;
